289b3e2d5d15b747c50ffa7b9d05f5b082e5ede0
IonasAndreea <Ionas.Va.Andreea@student.utcluj.ro>
2023-05-27 11:59:32 +0300

    update

--- a3/a3.c
+++ a3/a3.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include<string.h>
+#include <string.h>
+#include <sys/mman.h>
 
 
 typedef struct section_headers
@@ -18,6 +20,7 @@ typedef struct section_headers
 
 char buffer[100];
 int dim;
+char fileName[512];
 
 int main(int argc, char**argv)
 {
@@ -50,6 +53,9 @@ int main(int argc, char**argv)
         printf("SUCCESS\n");
     }
   
+   unsigned int nr;
+   volatile unsigned int *sharedMem = NULL;
+   unsigned int offset, value;
     while(1)
     {
 
@@ -64,16 +70,7 @@ int main(int argc, char**argv)
             }
             i++;
         }
-        //buffer[i-1] = '\0';
-        //size_t size = strlen(buffer);;
-        //read(fd, &dim, 1 );
-        //char* request = (char *) malloc(size* sizeof(char)) ;
-        //read(fd, request, length);
-        //if(size<= 0)
-        //{
-          //  break;
-        //}
-
+        
         if(strncmp(buffer, "EXIT#",5) == 0)
         {
             printf("EXIT\n");
@@ -82,15 +79,85 @@ int main(int argc, char**argv)
 
         if(strncmp(buffer, "VARIANT#",8) == 0)
         {
-            //write(fd2, &dim, 1);
+            
             write(fd2, "VARIANT#", 8);
-            //write(fd2, &dim,1);
             write(fd2, "VALUE#",6);
             unsigned int varianta = 35816;
             write(fd2, &varianta, sizeof(unsigned int));
         }
-        //free(request);
+
+        if(strncmp(buffer,"CREATE_SHM#",11) == 0)
+        {
+            read(fd,&nr,sizeof(unsigned int));
+            int shmFd = shm_open("/d5mpp1", O_CREAT | O_RDWR , 0664);
+            if (shmFd < 0)
+            {
+                write(fd2, "CREATE_SHM#", 11);
+                write(fd2, "ERROR#",6);
+                return 1;
+            }
+
+            ftruncate(shmFd, 2420777);
+            sharedMem = mmap(NULL, 2420777, PROT_READ | PROT_WRITE, MAP_SHARED,shmFd, 0);
+            if (sharedMem == (void*)-1)
+            {
+                write(fd2, "CREATE_SHM#", 11);
+                write(fd2, "ERROR#",6);
+                shm_unlink("/d5mpp1");
+                return 1;
+            }
+
+            write(fd2, "CREATE_SHM#", 11);
+            write(fd2, "SUCCESS#",8);
+        }
+
+        if(strncmp(buffer,"WRITE_TO_SHM#",13) == 0)
+        {
+            read(fd, &offset, sizeof(unsigned int));
+            read(fd, &value, sizeof(unsigned int));
+
+            if(offset <0 || offset >=2420777)
+            {
+                write(fd2, "WRITE_TO_SHM#", 13);
+                write(fd2, "ERROR#",6);
+            }
+            //*(sharedMem + offset)= value * sizeof(unsigned int);
+            unsigned int* pozitie = (unsigned int*)((char*)sharedMem + offset);
+            *pozitie = value;
+            write(fd2, "WRITE_TO_SHM#", 13);
+            write(fd2, "SUCCESS#",8);
+
+        }
+
+        if(strncmp(buffer, "MAP_FILE#",9) == 0)
+        {
+            int i= 0 ;
+            while(1)
+            {  
+                read(fd, &fileName[i], 1);
+                if(fileName[i] == '#' )
+                    {   
+                    break;
+                    }
+                i++;
+            }
+            int fd_f = open(fileName, O_RDONLY);
+            if(fd_f == -1)
+            {
+                write(fd2, "MAP_FILE#", 9);
+                write(fd2, "ERROR#",6);
+            }
+
+            write(fd2, "MAP_FILE#", 9);
+            write(fd2, "SUCCESS#",8);
+
+        }
+
+
+    
     }
+    munmap((void*)sharedMem, 2420777);
+    shm_unlink("/d5mpp1");
 
     remove("RESP_PIPE_35816");
     remove("REQ_PIPE_35816");
